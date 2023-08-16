51bda03aef180c476975ca43176d941fd2887b5c
Oksana Komarova <rabota_komarova@mail.ru>
2023-08-16 19:40:31 +0700

    feat(store): add initial mobx store

---
+++ src/store/todoStore.ts
@@ -0,0 +1,52 @@
+import { action, computed, makeObservable, observable } from 'mobx';
+import uniqid from 'uniqid';
+
+
+interface Todo {
+    id: string;
+    title: string;
+    completed: boolean;
+}
+
+class ToDoStore {
+    todos: Todo[] = [];
+
+    constructor() {
+        makeObservable(this, {
+            todos: observable,
+            totalTodosCount: computed,
+            completedTodosCount: computed,
+            addTodo: action,
+            remove: action,
+            completedTodo: action,
+        });
+    }
+
+    addTodo(todo:string) {
+        this.todos.push({
+            id: uniqid(),
+            title: todo,
+            completed: false,
+        });
+    }
+
+    get completedTodosCount() {
+        return this.todos.filter((todo) => todo.completed === true).length;
+    }
+
+    get totalTodosCount() {
+        return this.todos.length;
+    }
+
+    remove(id:string) {
+        this.todos = this.todos.filter((item) => item.id !== id);
+    }
+
+    completedTodo(id:string) {
+        this.todos = this.todos.map((item) =>
+            item.id === id ? { ...item, completed: !item.completed } : item,
+        );
+    }
+}
+
+export default new ToDoStore();
