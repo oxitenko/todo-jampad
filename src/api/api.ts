de524a58aae619684df8aeecfbb8c9164e2c1396
Oksana Komarova <rabota_komarova@mail.ru>
2023-08-17 21:02:46 +0700

    feat(gh-pages): add gh-pages

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,14 +1,9 @@
 import axios, {AxiosResponse} from "axios";
+import {IToDo} from "../store/todoStore";
 
-interface ITodo {
-    userId: number;
-    id: number;
-    title: string;
-    completed: boolean;
-}
-export async function fetchData(): Promise<ITodo[]> {
+export async function fetchData(): Promise<IToDo[]> {
     try {
-        const response: AxiosResponse<ITodo[]> = await axios.get('https://jsonplaceholder.typicode.com/users/1/todos');
+        const response: AxiosResponse<IToDo[]> = await axios.get('https://jsonplaceholder.typicode.com/users/1/todos');
         return response.data.slice(0, 5);
     } catch (error) {
         console.error('Error fetching todos:', error);
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import TodoList from "../ToDoList/ToDoList";
 
 function App() {
